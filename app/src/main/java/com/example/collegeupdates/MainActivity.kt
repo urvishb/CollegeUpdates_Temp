233ed0823725c57fa3afb8e824d77ae341e1a66f
Urvish <ubundela55@gmail.com>
2020-10-20 19:06:35 +0530

    this is our Fifth commit

--- app/src/main/java/com/example/collegeupdates/MainActivity.kt
+++ app/src/main/java/com/example/collegeupdates/MainActivity.kt
@@ -9,7 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is after the first commit")
-
-        this is intentional lines to produce errors
+        wow a new commit
     }
 }
