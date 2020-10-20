983eb7d83792a96f4b99aadc3b1531fae3a2110c
Urvish <ubundela55@gmail.com>
2020-10-20 19:06:56 +0530

    Revert "this is our Fifth commit"
    This reverts commit 233ed082

--- app/src/main/java/com/example/collegeupdates/MainActivity.kt
+++ app/src/main/java/com/example/collegeupdates/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is after the first commit")
-        wow a new commit
+
+        this is intentional lines to produce errors
     }
 }
