549e6594ba092c2c8d6a13633ce0d9cf52564076
Urvish <ubundela55@gmail.com>
2020-10-20 18:37:53 +0530

    This is our third commit

--- app/src/main/java/com/example/collegeupdates/MainActivity.kt
+++ app/src/main/java/com/example/collegeupdates/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is after the first commit")
+
+        all these lines are intentionally added to produce errors!
     }
 }
