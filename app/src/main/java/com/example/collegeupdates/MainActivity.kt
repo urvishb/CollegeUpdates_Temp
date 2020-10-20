b5f6ca4abb99909101a24b827fda181ea8495ac1
Urvish <ubundela55@gmail.com>
2020-10-20 19:10:27 +0530

    this is our sixth commit

--- app/src/main/java/com/example/collegeupdates/MainActivity.kt
+++ app/src/main/java/com/example/collegeupdates/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is after the first commit")
+        println("This is after the 5th commit but is meaningful")
     }
 }
