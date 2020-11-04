c5eaac161df34406460b35e4f05ad1afd60f2e7a
Urvish <ubundela55@gmail.com>
2020-11-04 17:42:57 +0530

    Bottom Navigation View added with 3 Empty fragments.

--- app/src/main/java/com/example/collegeupdates/MainActivity.kt
+++ app/src/main/java/com/example/collegeupdates/MainActivity.kt
@@ -2,20 +2,45 @@ package com.example.collegeupdates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("This is after the first commit")
-        println("Local changes")
+        val firstFragment = EventsFrag()
+        val secondFragment = NoticeFrag()
+        val thirdFragment = TransportFrag()
 
-        println("These Changes are change 5 cmd")
+        bottomNavigationView.setSelectedItemId(R.id.miEvents)
 
-        println("this line will be merged via command line!")
+        setCurrentFragment(firstFragment)
 
-        println("Hello from the other universe friend!")
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.miEvents -> setCurrentFragment(firstFragment)
+                R.id.miNotice -> setCurrentFragment(secondFragment)
+                R.id.miBuses -> setCurrentFragment(thirdFragment)
+            }
+            true
+        }
 
+        bottomNavigationView.getOrCreateBadge(R.id.miEvents).apply {
+            number = 4
+            isVisible = true
+        }
+
+        bottomNavigationView.getOrCreateBadge(R.id.miNotice).apply {
+            number = 2
+            isVisible = true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
